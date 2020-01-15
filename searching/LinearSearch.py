b4bf29f84ac2b1e4d0dbcf43fcbccf1b61dff20e
“SayyedulBappy” <38081373+SayyedulBappy@users.noreply.github.com>
2020-01-15 21:06:26 +0600

    corrected Linear Search

--- searching/LinearSearch.py
+++ searching/LinearSearch.py
@@ -1,25 +1,22 @@
 
 # function to do linear search
-def LinearSearch(list,item):
-    for i in range(0,len(list)):
-        if list[i] == item:
+def LinearSearch(inputList,item):
+    for i in range(0,len(inputList)):
+        if inputList[i] == item:
             return i
-        else:
-            return -1
-
+    return -1
 # create a list 
 inputList = list(map(int,input("Enter numbers to create a list: ").strip().split())) 
 
-# Search
+# Search item
 item = int(input("Enter item to search: "))
-LinearSearch(inputList,item)
 
 # display result
-while(True):
-    if LinearSearch(inputList,item) == -1:
-        print(f"{item} is not in the list")
-    else:
-        print(f"{item} is at position {LinearSearch(inputList,item)}")
+index = LinearSearch(inputList,item)
+if index != -1: 
+	print(f"{item} is at index {index}") 
+else: 
+	print(f"{item} is not in the list")
 
 
 
