1210db6c3bf35afd22eb2b0b2f5ada407885425f
“SayyedulBappy” <38081373+SayyedulBappy@users.noreply.github.com>
2020-01-15 19:49:07 +0600

    added linear search

---
+++ searching/LinearSearch.py
@@ -0,0 +1,25 @@
+
+# function to do linear search
+def LinearSearch(list,item):
+    for i in range(0,len(list)):
+        if list[i] == item:
+            return i
+        else:
+            return -1
+
+# create an unsorted  list 
+inputList = list(map(int,input("Enter numbers to create a list: ").strip().split())) 
+
+# Search
+item = int(input("Enter item to search: "))
+LinearSearch(inputList,item)
+
+# display result
+while(True):
+    if LinearSearch(inputList,item) == -1:
+        print(f"{item} is not in the list")
+    else:
+        print(f"{item} is at position {LinearSearch(inputList,item)}")
+
+
+
